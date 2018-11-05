6265534d7ac17488ceb4316dac632451f766d4f4
tduzan <tyler.duzan@percona.com>
2018-11-05 10:03:56 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ple18_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: ple18_demo
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
